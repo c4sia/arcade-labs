2cb8bae39413f1f34fbfa2844e6e37677fdcb070
casia <casia.enoiu@edu.uah.es>
2025-02-20 14:38:44 +0100

    funcion de la luna + sombra funcion del mensaje

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -23,18 +23,6 @@ def luna(x: int, y: int)-> None:
     arcade.draw_arc_filled(x+25, y-9, 20, 11, arcade.color.BLACK_OLIVE, 180, 360, 0)
     arcade.draw_arc_filled(x+25, y+49, 20, 10, arcade.color.BLACK_OLIVE, 180, 360, 0)
 
-#LUNA
-arcade.draw_circle_filled(290, 364, 77, arcade.color.ASH_GREY)
-arcade.draw_circle_filled(256, 395, 7, arcade.color.DAVY_GREY)
-arcade.draw_circle_filled(256, 325, 18, arcade.color.DAVY_GREY)
-arcade.draw_circle_filled(315, 420, 13, arcade.color.DAVY_GREY)
-arcade.draw_circle_filled(315, 360, 12, arcade.color.DAVY_GREY)
-arcade.draw_circle_filled(345, 330, 8, arcade.color.DAVY_GREY)
-arcade.draw_arc_filled(255, 315, 26, 16, arcade.color.BLACK_OLIVE, 180, 360, 0)
-arcade.draw_arc_filled(345, 327, 12, 10, arcade.color.BLACK_OLIVE, 180, 360, 0)
-arcade.draw_arc_filled(315, 355, 20, 11, arcade.color.BLACK_OLIVE, 180, 360, 0)
-arcade.draw_arc_filled(315, 413, 20, 10, arcade.color.BLACK_OLIVE, 180, 360, 0)
-
 
 arcade.draw_lrtb_rectangle_filled(0, 800, 100, 0, arcade.color.DARK_MIDNIGHT_BLUE)
 
@@ -54,7 +42,9 @@ def escribir_mensaje(mensaje: str, x: int, y: int, tamaño_letra: int) -> None:
     """
     arcade.draw_text(mensaje, x, y, arcade.color.WHITE, tamaño_letra)
 
-escribir_mensaje("leon atilano gonzalez sotos", 40, 50, 80)
+escribir_mensaje("leon atilano gonzalez sotos", 40, 50, 6)
 sombra_luna(250, 100, 6)
+luna(290, 364)
+
 arcade.finish_render()
 arcade.run()
